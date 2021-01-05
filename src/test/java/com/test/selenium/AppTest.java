0e59264759bbad4c64630a1b37080b2b2edacf1d
REKHA <REKHA@LAPTOP-LC84DQCI>
2021-01-05 20:57:21 +0530

    Test Initial Commit

---
+++ src/test/java/com/test/selenium/AppTest.java
@@ -0,0 +1,38 @@
+package com.test.selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	WebDriver driver;
+
+ 
+ @BeforeTest
+  public void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+      driver = new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+      driver.get("http://3.17.68.25:32770/");
+  }
+  @Test
+  public void verifyHomepageTitle() {
+      driver.findElement(By.id("About Us")).click();
+ }
+  @AfterTest
+  public void terminateBrowser(){
+     // driver.close();
+  }
+}
